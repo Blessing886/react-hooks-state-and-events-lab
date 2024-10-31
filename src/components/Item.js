7d8de0f6167bcea38a49f84dcaf9edcc1e94ccba
BlessingOkora <moraablessing082@gmail.com>
2024-10-31 08:51:57 +0300

    Modifies cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, SetInCart] = useState(false);
+
+  const handleCartToggle = () => {
+    SetInCart((prevInCart) => !prevInCart);
+  };
+
+  const itemClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCartToggle} className="add">{inCart ? "Remove From Cart" : "Add to Cart" }</button>
     </li>
   );
 }
